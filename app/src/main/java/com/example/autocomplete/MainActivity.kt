4f0557e716c5a2881ad9710f7b59228f93cc1ffe
heeseon99 <gmltjs418@gmail.com>
2023-01-18 16:32:51 +0900

    자동완성 텍스트뷰
    - view binding으로 변경

--- app/src/main/java/com/example/autocomplete/MainActivity.kt
+++ app/src/main/java/com/example/autocomplete/MainActivity.kt
@@ -5,22 +5,28 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.MultiAutoCompleteTextView
+import com.example.autocomplete.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
         var items = arrayOf("CSI-뉴욕", "CSI-라스베가스", "CSI-마이애미",
             "Friends", "Fringe", "Lost")
 
-        var auto = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView1)
+        var auto = binding.autoCompleteTextView1
         var adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, items)
         auto.setAdapter(adapter)
 
-        var multi = findViewById<MultiAutoCompleteTextView>(
-            R.id.multiAutoCompleteTextView1)
+        var multi = binding.multiAutoCompleteTextView1
         var token = MultiAutoCompleteTextView.CommaTokenizer()
         multi.setTokenizer(token)
         multi.setAdapter(adapter)
     }
 }
+
